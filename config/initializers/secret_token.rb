b1c24e1d0ea44a917a76ad9bc97aa43930960f6d
Sasha Matviy <omatviy@gmail.com>
2014-06-07 19:10:24 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PazyukStat::Application.config.secret_token = '83b62c4ea06efe6175301aa6dff779ceebecf3a6989738502352f9ef9a27994da9cedd69faf8124ebb91ae35bf00b044c9502154dca5c8b044c71346387640eb'
